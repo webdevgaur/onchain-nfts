efa4a397b2f7415f4a707ccca15944bd6860282b
Anurag Gaur <anuraggaur495@gmail.com>
2022-11-19 22:33:19 +0530

    characterpanel introduced

---
+++ pages/components/CharacterPanel/CharacterPanel.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function CharacterPanel() {
+    
+    const wizardBaseSvg = ``;
+
+  return (
+    <div className='panel-wrapper'>
+        <div className="wizard">
+
+        </div>
+        <div className="warrior">
+
+        </div>
+        <div className="edgelord">
+
+        </div>
+    </div>
+  )
+}
+
+export default CharacterPanel
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import CharacterPanel from './components/CharacterPanel/CharacterPanel'
 
 export default function Home() {
   return (
-    <div>Hello world</div>
+    <>
+      <div>Hello world</div>
+      <CharacterPanel />
+    </>
+    
   )
 }
