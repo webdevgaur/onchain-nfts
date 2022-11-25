5633b7fbb258e407bb4b1cff8d59957145f932c2
Anurag Gaur <anuraggaur495@gmail.com>
2022-11-25 19:04:56 +0530

    merge conflict resolved

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,17 +7,10 @@ const Navbar = () => {
     <div className='p-5  flex flex-wrap sm:justify-between justify-center items-center border-b  border-gray-200'>
         <div className="flex justify-between items-center space-x-5 w-screen ">
         <p className="text-2xl  font-bold py-1 px-2 rounded">
-<<<<<<< HEAD
-          Multi Player 🎮 
-        </p>
-        {!currentAccount ? (
- 
-=======
           Multi Player 🎮
         </p>
         {!currentAccount ? (
 
->>>>>>> 81bcc73118b02894c4ea77430083ce23a4bdbf2e
             <button type="button" onClick={() => connectWallet()}  className="text-xl  border rounded-full px-4 py-2  bg-violet-700 text-white hover:shadow-sm "> Connect Walllet</button>
         ): (
             <div className='bg-gray-100 px-4 py-3 rounded-md'>
