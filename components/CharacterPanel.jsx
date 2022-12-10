2d76180e39ef091c66000493ffeec783b05f7ba9
Anurag Gaur <anuraggaur495@gmail.com>
2022-12-10 19:29:56 +0530

    new character svg

--- components/CharacterPanel.jsx
+++ components/CharacterPanel.jsx
@@ -43,3 +43,11 @@ function CharacterPanel() {
 }
 
 export default CharacterPanel
+
+<svg width="1238" height="1238" viewBox="0 0 1238 1238" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="12.5" y="12.5" width="1213" height="1213" rx="60.5" fill="#DAFF9D" stroke="#434343" stroke-width="25"/>
+<circle cx="240.517" cy="367.772" r="105.434" fill="#FFDAAE"/>
+<path d="M240.517 59L349.218 332H131.816L240.517 59Z" fill="#C29DFF"/>
+<rect x="453.896" y="239.745" width="25.1034" height="532.193" fill="#7D350D"/>
+<rect x="130.062" y="520.903" width="223.421" height="223.421" fill="#C29DFF"/>
+</svg>
